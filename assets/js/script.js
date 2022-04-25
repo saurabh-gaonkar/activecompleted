362e07d91b2399d9ebf69ac910be6a6ee3361081
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-25 15:29:36 +0530

    js for cleare completed functionality in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,41 @@
 let serchbar = document.querySelector(".searchbar"),
 dropper = document.querySelector(".dropper"),
-searchlist = document.querySelector(".searchlist");
+searchlist = document.querySelector(".searchlist"),
+all = document.querySelector(".all"),
+active = document.querySelector(".active"),
+completed = document.querySelector(".completed"),
+clearcompleted = document.querySelector(".clearcompleted");
+
+
+const createlist = () =>{
+    searchlist.innerHTML = "";
+    let getsearchitems = JSON.parse(localStorage.getItem("searchitems"));
+getsearchitems.forEach(i=>{
+    if(i.checked === true){
+    searchlist.innerHTML += `<li><input type="checkbox" checked="true" class="checkedbox">${i.name}</li>`
+    }else {
+        searchlist.innerHTML += `<li><input type="checkbox" class="checkedbox">${i.name}</li>`
+    }
+})
+
+}
+
+
+
+const showlist = () => {
+    createlist();
+    let checkboxes = document.querySelectorAll(".checkedbox");
+  searchlist.classList.toggle("active");
+  checkboxes.forEach((i,index)=>{
+      i.addEventListener("click",()=>{
+        let searchitems = JSON.parse(localStorage.getItem("searchitems"));
+        searchitems[index].checked=true;
+        localStorage.setItem("searchitems",JSON.stringify(searchitems));
+      })
+  })
+}
+
+
 serchbar.addEventListener("keyup",(e)=>{
     if(e.keyCode === 13) {
         if (!(localStorage.getItem("searchitems"))) {
@@ -9,21 +44,57 @@ serchbar.addEventListener("keyup",(e)=>{
 
         let searchitems = JSON.parse(localStorage.getItem("searchitems")),
         newitem = serchbar.value;
-        searchitems.push(newitem);
+        falsevalue = false;
+        searchitems.forEach(i=>{
+            if(i === newitem || newitem === "") {
+                falsevalue = true;
+            }
+        })
+        if(falsevalue === false) {
+        newobject = {name:newitem,checked:false};
+        searchitems.push(newobject);
         localStorage.setItem("searchitems",JSON.stringify(searchitems));
+        }
+        showlist();
     }
-
 })
 
-let getsearchitems = JSON.parse(localStorage.getItem("searchitems"));
+dropper.addEventListener("click",showlist);
+
+all.addEventListener("click",showlist);
+
+active.addEventListener("click",()=>{
+    searchlist.innerHTML = "";
+    let getsearchitems = JSON.parse(localStorage.getItem("searchitems"));
 getsearchitems.forEach(i=>{
-    searchlist.innerHTML += `<option><input type="checkbox">${i}</option>`
+    if(i.checked === false){
+    searchlist.innerHTML += `<li><input type="checkbox" class="checkedbox">${i.name}</li>`
+    }
+})
+searchlist.classList.add("active");
 })
 
-// dropper.addEventListener("click",()=>{
-    
-// })
+completed.addEventListener("click",()=>{
+    searchlist.innerHTML = "";
+    let getsearchitems = JSON.parse(localStorage.getItem("searchitems"));
+getsearchitems.forEach(i=>{
+    if(i.checked === true){
+    searchlist.innerHTML += `<li><input type="checkbox" checked="true" class="checkedbox">${i.name}</li>`
+    }
+})
+searchlist.classList.add("active");
+})
 
+clearcompleted.addEventListener("click",()=>{
+    searchlist.innerHTML = "";
+    let getsearchitems = JSON.parse(localStorage.getItem("searchitems"));
+getsearchitems.forEach((i,index)=>{
+    if(i.checked === true){
+        getsearchitems.splice(index,1);
+    }
+})
+searchlist.classList.add("active");
+})
 
 
 
