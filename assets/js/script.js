138b3adc7bf1192bbda9bc7f4c276aa71637da90
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-25 12:36:53 +0530

    js code for dynamic datalist data in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-/* Author: 
+let serchbar = document.querySelector(".searchbar"),
+dropper = document.querySelector(".dropper"),
+searchlist = documen
+serchbar.addEventListener("keyup",(e)=>{
+    if(e.keyCode === 13) {
+        if (!(localStorage.getItem("searchitems"))) {
+            localStorage.setItem("searchitems", '[]');
+        }
 
-*/
+        let searchitems = JSON.parse(localStorage.getItem("searchitems")),
+        newitem = serchbar.value;
+        searchitems.push(newitem);
+    }
+})
 
+.dropper.addEventListener("click",()=>{
 
-
-
-
-
-
-
+})
 
 
 
